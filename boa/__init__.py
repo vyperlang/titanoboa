89da3b5cc7bcf2c27d6a8db0a2e197f027134f2c
Charles Cooper <cooper.charles.m@gmail.com>
2022-07-14 21:49:05 -0400

    fix lint

--- boa/__init__.py
+++ boa/__init__.py
@@ -1,7 +1,7 @@
 import sys
 
 from boa.env import Env
-from boa.interpret import contract, load, loads, loads_partial, load_partial
+from boa.interpret import contract, load, load_partial, loads, loads_partial
 
 # turn off tracebacks if we are in repl
 # https://stackoverflow.com/a/64523765
