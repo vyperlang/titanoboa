bc90de54a7a965588e767f250bd8a5625baaf3f3
Charles Cooper <cooper.charles.m@gmail.com>
2022-07-14 21:41:41 -0400

    add partial contract loading

--- boa/interpret.py
+++ boa/interpret.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from vyper.compiler.phases import CompilerData
 
-from boa.contract import VyperContract, VyperFactory
+from boa.contract import VyperContract, VyperDeployer, VyperFactory
 
 _Contract = Union[VyperContract, VyperFactory]
 
@@ -12,12 +12,23 @@ def load(filename: str, *args, **kwargs) -> _Contract:  # type: ignore
         return loads(f.read(), *args, **kwargs)
 
 
-def loads(source_code: str, *args, as_factory=False, **kwargs) -> _Contract:  # type: ignore
+def loads_partial(source_code: str) -> VyperDeployer:
     data = CompilerData(source_code, no_optimize=True)
+
+    return VyperDeployer(data)
+
+
+def load_partial(filename: str, *args, **kwargs) -> VyperDeployer:  # type: ignore
+    with open(filename) as f:
+        return loads_partial(f.read(), *args, **kwargs)
+
+
+def loads(source_code: str, *args, as_factory=False, **kwargs) -> _Contract:  # type: ignore
+    d = loads_partial(source_code)
     if as_factory:
-        return VyperFactory(data, **kwargs)
+        return d.deploy_as_factory(**kwargs)
     else:
-        return VyperContract(data, *args, **kwargs)
+        return d.deploy(*args, **kwargs)
 
 
 def contract() -> _Contract:
