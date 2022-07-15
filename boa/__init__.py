87f2a1eff37d67cb3ba8b736940a738cb64be5b2
Charles Cooper <cooper.charles.m@gmail.com>
2022-07-15 16:11:22 -0400

    remove set_cache_dir from boa interface

--- boa/__init__.py
+++ boa/__init__.py
@@ -1,7 +1,7 @@
 import sys
 
 from boa.env import Env
-from boa.interpret import contract, load, load_partial, loads, loads_partial, set_cache_dir
+from boa.interpret import contract, load, load_partial, loads, loads_partial
 
 # turn off tracebacks if we are in repl
 # https://stackoverflow.com/a/64523765
