c77576d18916b6edcc182d42a7a79618d5ed999a
Charles Cooper <cooper.charles.m@gmail.com>
2022-07-19 11:04:15 -0400

    clean a comment

--- boa/ipython.py
+++ boa/ipython.py
@@ -5,6 +5,7 @@ import boa
 _ = None
 _contracts = {}
 
+
 @ipython.magics_class
 class TitanoboaMagic(ipython.Magics):
     @ipython.cell_magic
@@ -13,9 +14,10 @@ class TitanoboaMagic(ipython.Magics):
         c = boa.loads_partial(cell, name=line)
         if line:
             self.shell.user_ns[line] = c  # ret available in user ipython locals
-            _contracts[line] = c  # ret available at boa.ipython._contracts["..."]
+            _contracts[line] = c  # ret available in boa.ipython._contracts
         _ = c  # ret available at `boa.ipython._`
         return c
+
 
 def load_ipython_extension(ipy_module):
     ipy_module.register_magics(TitanoboaMagic)
