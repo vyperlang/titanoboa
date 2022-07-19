ea3502f4ea63365138af3311858135787d0018e4
z80 <z80dev@mlnl.finance>
2022-07-19 10:42:25 -0400

    Merge orgvyper + vyper fns, implement name binding

--- boa/ipython.py
+++ boa/ipython.py
@@ -1,7 +1,10 @@
 import IPython.core.magic as ipython
 
 import boa
+import sys
 
+_ = None
+_contracts = {}
 
 # The class MUST call this class decorator at creation time
 @ipython.magics_class
@@ -11,16 +14,11 @@ class TitanoboaMagic(ipython.Magics):
         # note use of eval here is ok since everything is being
         # eval'ed anyway.
         c = boa.loads_partial(cell)
-        return c
-
-    @ipython.cell_magic
-    def orgvyper(self, line, cell):
-        # note use of eval here is ok since everything is being
-        # eval'ed anyway.
-        c = boa.loads_partial(cell)
         if line:
-            boa.env.contracts[line] = c
+            self.shell.user_ns[line] = c
+            _contracts[line] = c
+        _ = c
         return c
 
 def load_ipython_extension(ipy_module):
-    ipy_module.register_magics(TitanoboaMagic())
+    ipy_module.register_magics(TitanoboaMagic)
