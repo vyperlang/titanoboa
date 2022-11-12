8028569315a5696ee1423785b436848843b44288
bout3fiddy <11488427+bout3fiddy@users.noreply.github.com>
2022-11-12 12:11:58 +0100

    test ignore_isolation custom marker

--- boa/test/plugin.py
+++ boa/test/plugin.py
@@ -2,7 +2,14 @@ import pytest
 import boa
 
 
+def pytest_configure(config):
+    config.addinivalue_line("markers", "ignore_isolation")
+
+
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_call(item):
-    with boa.env.anchor():
+    if not item.get_closest_marker("ignore_isolation"):
+        with boa.env.anchor():
+            item.runtest()
+    else:
         item.runtest()
--- tests/test/test_isolation.py
+++ tests/test/test_isolation.py
@@ -1,3 +1,4 @@
+import pytest
 import boa
 
 from boa.test import given, strategy
@@ -25,6 +26,7 @@ with boa.env.prank(boa.env.generate_address()):
 
 A_INIT = boa_contract.a()
 B_INIT = boa_contract.b()
+addr_const = boa.env.generate_address()
 
 
 @given(a=strategy("uint"), b=strategy("address"))
@@ -32,3 +34,19 @@ def test_state_isolation(a, b):
     assert boa_contract.a() == A_INIT
     assert boa_contract.b() == B_INIT
     boa_contract.set_vars(a, b)
+    assert boa_contract.a() == a
+    assert boa_contract.b() == b
+
+
+@pytest.mark.ignore_isolation
+def test_ignore_isolation_init():
+    assert boa_contract.a() == A_INIT
+    assert boa_contract.b() == B_INIT
+    boa_contract.set_vars(42069, addr_const)
+
+
+@pytest.mark.ignore_isolation
+def test_check_ignore_isolation():
+    print(boa_contract.a())
+    assert boa_contract.a() == 42069
+    assert boa_contract.b() == addr_const
