9e426e1a1305948c66768663d6f398f5d51bec65
Edward Amor <edward.amor3@gmail.com>
2022-11-23 11:49:03 -0500

    docs: setup sphinx extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,11 @@ release = "0.1.6"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = []
@@ -24,3 +28,14 @@ exclude_patterns = []
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+
+# -- Options for autosectionlabel extension ----------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/autosectionlabel.html
+
+autosectionlabel_prefix_document = True
+autosectionlabel_maxdepth = 3
+
+# -- Options for intersphinx extension ---------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html
+
+intersphinx_mapping = {"pyevm": ("https://py-evm.readthedocs.io/en/latest", None)}
