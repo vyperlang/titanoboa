19946af6cda856f04a9ae21e634b2a186a2db62b
Charles Cooper <cooper.charles.m@gmail.com>
2022-11-24 16:09:02 -0800

    add test for dynamic injection

---
+++ tests/unitary/test_injects.py
@@ -0,0 +1,33 @@
+import pytest
+from hypothesis import given
+from boa.test.strategies import strategy
+import boa
+
+contract_code = """
+totalSupply: public(uint256)
+"""
+
+inject_code = """
+@external
+def test_mint(amt: uint256):
+    self.totalSupply += amt
+"""
+
+@pytest.fixture(scope="module")
+def contract():
+    s = boa.loads(contract_code)
+    s.inject_function(inject_code)
+    return s
+
+def test_inject_force(contract):
+    with pytest.raises(ValueError):
+        # name collision
+        contract.inject_function(inject_code)
+
+    contract.inject_function(inject_code, force=True)
+ 
+ 
+@given(x=strategy("uint256"))
+def test_inject(contract, x):
+    contract.inject.test_mint(x)
+    assert contract.totalSupply() == x
