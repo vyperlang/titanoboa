3346a0b9c8a51c2260ff53e19854ddafbdcd192c
bout3fiddy <11488427+bout3fiddy@users.noreply.github.com>
2022-12-17 12:06:31 +0530

    add contract name to table

--- tests/unitary/test_call_profiling/test_call_profile.py
+++ tests/unitary/test_call_profiling/test_call_profile.py
@@ -22,6 +22,12 @@ def bar(a: uint256, b: uint256) -> uint256:
 @view
 def baz(c: address):
     assert c != empty(address)
+
+
+@external
+@view
+def sip():
+    assert 10 > 0
 """
 
 
@@ -31,7 +37,7 @@ def boa_contract():
 
 
 @pytest.mark.parametrize("a,b", [(42, 69), (420, 690), (42, 690), (420, 69)])
-# @pytest.mark.profile_calls
+@pytest.mark.profile_calls
 def test_populate_call_profile_property(boa_contract, a, b):
 
     boa_contract.foo(a, b)
@@ -41,3 +47,35 @@ def test_populate_call_profile_property(boa_contract, a, b):
 @pytest.mark.profile_calls
 def test_append_to_pytest_call_profile(boa_contract):
     boa_contract.baz(boa.env.generate_address())
+
+    assert "TestContract.foo" in boa.env.profiled_calls.keys()
+    assert "TestContract.bar" in boa.env.profiled_calls.keys()
+
+
+def test_ignore_call_profiling(boa_contract):
+    boa_contract.sip()
+
+    assert "TestContract.sip" not in boa.env.profiled_calls.keys()
+
+
+@pytest.mark.profile_calls
+@pytest.mark.parametrize(
+    "a,b,c", [(42, 69, 1), (420, 690, 20), (42, 690, 10), (420, 69, 5)]
+)
+def test_call_stdev(a, b, c):
+
+    source_code = """
+N_ITER: constant(uint256) = 0x00001
+
+@external
+@view
+def foo(a: uint256, b: uint256) -> uint256:
+    d: uint256 = 0
+    for j in range(N_ITER):
+        d = unsafe_mul(d, isqrt(unsafe_div(a, b) + unsafe_mul(a, b)))
+    return d
+"""
+    source_code = source_code.replace("0x00001", str(c))
+    boa_contract = boa.loads(source_code, name="TestVariableLoopContract")
+
+    boa_contract.foo(a, b)
