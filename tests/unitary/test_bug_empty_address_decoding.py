e53e328d31b48e2bd6a831ab521f5219b6d86f98
bout3fiddy <11488427+bout3fiddy@users.noreply.github.com>
2022-12-29 14:02:04 +0100

    improve buggy test. seems to be with addresses only

--- tests/unitary/test_bug_empty_address_decoding.py
+++ tests/unitary/test_bug_empty_address_decoding.py
@@ -4,44 +4,38 @@ import boa
 
 
 @pytest.fixture(scope="module")
-def boa_contract_throws_error():
+def boa_contract_a():
     source_code = """
 @external
 def test(i: uint256, j: uint256) -> uint256:
     assert i != j  # dev: lorem ipsum
 
-    _test: address[3] = empty(address[3])
+    _b: address = 0x809BAf72e430abD10044fF569Bebe87d466d90c9
     return 0
 """
     return boa.loads(source_code)
 
 
 @pytest.fixture(scope="module")
-def boa_contract_works():
+def boa_contract_b():
     source_code = """
 @external
 def test(i: uint256, j: uint256) -> uint256:
     assert i != j  # dev: lorem ipsum
 
-    _test: uint256[3] = empty(uint256[3])
+    _b: address = empty(address)
     return 0
 """
     return boa.loads(source_code)
 
 
-def test_revert_throws_valueerror(boa_contract_throws_error):
-
-    with pytest.raises(ValueError) as caught_exception:
-        with boa.reverts(), boa.env.prank(boa.env.generate_address()):
-            boa_contract_throws_error.test(0, 0)
-
-    assert (
-        caught_exception.value.args[0]
-        == "Unknown format b'', attempted to normalize to '0x'"
-    )
-
-
-def test_revert_works(boa_contract_works):
+def test_bug_with_nonempty_address(boa_contract_a):
 
     with boa.reverts(), boa.env.prank(boa.env.generate_address()):
-        boa_contract_works.test(0, 0)
+        boa_contract_a.test(0, 0)
+
+
+def test_bug_with_address(boa_contract_b):
+
+    with boa.reverts(), boa.env.prank(boa.env.generate_address()):
+        boa_contract_b.test(0, 0)
