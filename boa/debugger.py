8bbbbf2af4e25ce262c011ccf99e7ff27d173079
fiddy <11488427+bout3fiddy@users.noreply.github.com>
2023-04-08 16:36:34 +0200

    fix: decoding of bytesM types (#68)

--- boa/debugger.py
+++ boa/debugger.py
@@ -2,6 +2,7 @@ import cmd
 
 from vyper.exceptions import VyperException
 
+
 class BoaDebug(cmd.Cmd):
     prompt = "(boa-debug) "
 
@@ -12,7 +13,7 @@ class BoaDebug(cmd.Cmd):
         self.contract._computation = computation
 
         ast_source = self.contract.find_source_of(computation.code)
-        if ast_source is not None:        
+        if ast_source is not None:
             print(VyperException("breakpoint at:", ast_source), file=self.stdout)
 
     def do_show_contract(self, *args):
