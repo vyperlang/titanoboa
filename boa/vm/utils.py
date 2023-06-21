d7ab2ca569261affaaa30bd3a9fd8822b630c368
Charles Cooper <cooper.charles.m@gmail.com>
2023-06-20 19:10:26 -0700

    fix bytes padding

--- boa/vm/utils.py
+++ boa/vm/utils.py
@@ -10,5 +10,5 @@ def to_int(stack_item) -> int:
 
 def to_bytes(stack_item) -> bytes:
     if isinstance(stack_item, bytes):
-        return stack_item
+        return stack_item.rjust(32, b"\x00")
     return stack_item.to_bytes(32, "big")
