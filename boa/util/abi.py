dd4da06bb96e5bb7abc274bfaefb128693e28a0d
Charles Cooper <cooper.charles.m@gmail.com>
2023-06-22 20:14:55 -0700

    forgot to commit abi util file,
    should squash with bd9bb72b6a9b2c5d324b367e44df30b38b65020d

---
+++ boa/util/abi.py
@@ -0,0 +1,23 @@
+# wrapper module around whatever encoder we are using
+
+from eth.codecs.abi.decoder import Decoder
+from eth.codecs.abi.encoder import Encoder
+from eth.codecs.abi.parser import Parser
+
+_parsers = {}
+
+
+def _get_parser(schema):
+    try:
+        return _parsers[schema]
+    except KeyError:
+        _parsers[schema] = (ret := Parser.parse(schema))
+        return ret
+
+
+def abi_encode(schema, data):
+    return Encoder.encode(_get_parser(schema), data)
+
+
+def abi_decode(schema, data):
+    return Decoder.decode(_get_parser(schema), data)
