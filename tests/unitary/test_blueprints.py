44c14406eda4fefc0dad99a287dff7fae2172a18
Charles Cooper <cooper.charles.m@gmail.com>
2023-09-08 15:59:49 -0400

    fix lint

--- tests/unitary/test_blueprints.py
+++ tests/unitary/test_blueprints.py
@@ -1,19 +1,21 @@
+from eth_utils import to_canonical_address
+
 import boa
 from boa.util.eip5202 import get_create2_address
-from eth_utils import to_canonical_address
 
 blueprint_code = """
 @external
 def some_function() -> uint256:
     return 5
-"""         
-            
+"""
+
 factory_code = """
-@external   
+@external
 def create_child(blueprint: address, salt: bytes32) -> address:
     return create_from_blueprint(blueprint, code_offset=3, salt=salt)
-"""         
-            
+"""
+
+
 def test_create2_address():
     blueprint = boa.loads_partial(blueprint_code).deploy_as_blueprint()
     factory = boa.loads(factory_code)
@@ -23,5 +25,9 @@ def test_create2_address():
     child_contract_address = factory.create_child(blueprint.address, salt)
 
     # TODO: make a util function on boa.env to get code
-    blueprint_bytecode = boa.env.vm.state.get_code(to_canonical_address(blueprint.address))
-    assert child_contract_address == get_create2_address(blueprint_bytecode, factory.address, salt)
+    blueprint_bytecode = boa.env.vm.state.get_code(
+        to_canonical_address(blueprint.address)
+    )
+    assert child_contract_address == get_create2_address(
+        blueprint_bytecode, factory.address, salt
+    )
