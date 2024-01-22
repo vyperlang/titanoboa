0bd8ee8dd75e58b95bee4b527e2f4f7ff495e5f8
mo <anonmo427@gmail.com>
2024-01-22 15:17:36 +0100

    cleanup

--- boa/explorer.py
+++ boa/explorer.py
@@ -18,30 +18,27 @@ def fetch_abi_from_etherscan(
 
     res = SESSION.get(uri, params=params)
     res.raise_for_status()
+    source_data = res.json()
 
-    data = res.json()
-
-    if int(data["status"]) != 1:
+    if int(source_data["status"]) != 1:
         raise ValueError(f"Failed to retrieve data from API: {data}")
 
-    data = data["result"][0]
+    source_data = source_data["result"][0]
 
-    if "Proxy" in data and int(data["Proxy"]) == 1:
-        address = data.get("Implementation")
-    else:
-        address = address
+    # check if the contract is a proxy
+    if int(source_data["Proxy"]) == 1:
+        address = source_data.get("Implementation")
 
+    # fetch ABI of `address`
     params = dict(module="contract", action="getabi", address=address)
     if api_key is not None:
         params["apikey"] = api_key
 
     res = SESSION.get(uri, params=params)
     res.raise_for_status()
-
     data = res.json()
 
     if int(data["status"]) != 1:
         raise ValueError(f"Failed to retrieve data from API: {data}")
 
     return json.loads(data["result"].strip())
- 
