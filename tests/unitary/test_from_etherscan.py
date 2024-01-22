0ab44b6ce655f84ee97313f5b7ef0254d81d4f07
mo <anonmo427@gmail.com>
2024-01-22 22:38:57 +0100

    add asserts to tests

--- tests/unitary/test_from_etherscan.py
+++ tests/unitary/test_from_etherscan.py
@@ -1,36 +1,25 @@
-import boa, os
+import os
 
-crvusd = '0xf939E0A03FB07F59A73314E73794Be0E57ac1b4E'
+import boa
+
+crvusd = "0xf939E0A03FB07F59A73314E73794Be0E57ac1b4E"
 voting_agent = "0xE478de485ad2fe566d49342Cbd03E49ed7DB3356"
 
 
-def test_from_etherscan_abi():
-
-    boa.env.fork(os.environ["ALCHEMY_MAINNET_ENDPOINT"])
-    abi = boa.from_etherscan_abi(crvusd, api_key=os.environ["ETHERSCAN_API_KEY"])
-
-    return abi
-
-
 def test_from_etherscan():
-
     boa.env.fork(os.environ["ALCHEMY_MAINNET_ENDPOINT"])
-    crvusd =  boa.from_etherscan(crvusd, api_key=os.environ["ETHERSCAN_API_KEY"])
-    
-    return crvusd.name()
+    contract = boa.from_etherscan(crvusd, api_key=os.environ["ETHERSCAN_API_KEY"])
+
+    assert contract.totalSupply() > 0
+    assert contract.symbol() == "crvUSD"
 
 
 def test_proxy_contract():
-
     boa.env.fork(os.environ["ALCHEMY_MAINNET_ENDPOINT"])
-    contract =  boa.from_etherscan_abi(voting_agent, api_key=os.environ["ETHERSCAN_API_KEY"])
+    contract = boa.from_etherscan(voting_agent, api_key=os.environ["ETHERSCAN_API_KEY"])
 
-    return contract.getVote(100)
+    assert contract.minTime == 43200
 
 
-def test_proxy_contract_abi():
-
-    boa.env.fork(os.environ["ALCHEMY_MAINNET_ENDPOINT"])
-    contract =  boa.from_etherscan_abi(voting_agent, api_key=os.environ["ETHERSCAN_API_KEY"])
-
-    return contract
+test_from_etherscan()
+test_proxy_contract()
