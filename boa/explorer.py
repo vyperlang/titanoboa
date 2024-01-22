b478df8c9e81ff07ccf7e4f271ab0df1162ca811
mo <anonmo427@gmail.com>
2024-01-22 22:34:22 +0100

    seperate function to get implementation address

--- boa/explorer.py
+++ boa/explorer.py
@@ -10,24 +10,8 @@ SESSION = requests.Session()
 def fetch_abi_from_etherscan(
     address: str, uri: str = "https://api.etherscan.io/api", api_key: str = None
 ):
-    address = Address(address)
-
-    params = dict(module="contract", action="getsourcecode", address=address)
-    if api_key is not None:
-        params["apikey"] = api_key
-
-    res = SESSION.get(uri, params=params)
-    res.raise_for_status()
-    source_data = res.json()
-
-    if int(source_data["status"]) != 1:
-        raise ValueError(f"Failed to retrieve data from API: {data}")
-
-    source_data = source_data["result"][0]
-
-    # check if the contract is a proxy
-    if int(source_data["Proxy"]) == 1:
-        address = source_data.get("Implementation")
+    # check if `address` is a proxy contract
+    address = Address(_get_implementation_address(address, uri, api_key))
 
     # fetch ABI of `address`
     params = dict(module="contract", action="getabi", address=address)
@@ -42,3 +26,26 @@ def fetch_abi_from_etherscan(
         raise ValueError(f"Failed to retrieve data from API: {data}")
 
     return json.loads(data["result"].strip())
+
+
+def _get_implementation_address(
+    address: str, uri: str = "https://api.etherscan.io/api", api_key: str = None
+):
+    params = dict(module="contract", action="getsourcecode", address=address)
+    if api_key is not None:
+        params["apikey"] = api_key
+
+    res = SESSION.get(uri, params=params)
+    res.raise_for_status()
+    source_data = res.json()
+
+    if int(source_data["status"]) != 1:
+        raise ValueError(f"Failed to retrieve source from Etherscan: {source_data}")
+
+    source_data = source_data["result"][0]
+
+    # check if the contract is a proxy
+    if int(source_data["Proxy"]) == 1:
+        return source_data.get("Implementation")
+    else:
+        return address
