0a8640f049f8b4703f09b998eaef36be01be0316
Daniel Schiavini <daniel.schiavini@gmail.com>
2024-03-06 20:57:06 +0100

    Fix test

--- tests/unitary/utils/test_lrudict.py
+++ tests/unitary/utils/test_lrudict.py
@@ -1,9 +1,17 @@
 from boa.util.lrudict import lrudict
 
 
-def test_lru():
+def test_lru_setdefault():
     d = lrudict(10)
     for x in range(20):
         d.setdefault_lambda(x, lambda k: x)
-        d.setdefault_lambda(x, lambda k: x * 100)
+        d.setdefault_lambda(x, lambda k: x * 100)  # no effect
+    assert d == {k: k for k in range(10, 20)}
+
+
+def test_lru_set():
+    d = lrudict(10)
+    for x in range(20):
+        d.setdefault_lambda(x, lambda k: x)
+        d[x] = x * 100
     assert d == {k: k * 100 for k in range(10, 20)}
