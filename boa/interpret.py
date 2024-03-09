9cf289ff06b1dc6052f682caa32ab68a8669f879
Charles Cooper <cooper.charles.m@gmail.com>
2024-03-09 14:36:01 -0500

    fix caching for modules

--- boa/interpret.py
+++ boa/interpret.py
@@ -5,12 +5,19 @@ from importlib.abc import MetaPathFinder
 from importlib.machinery import SourceFileLoader
 from importlib.util import spec_from_loader
 from pathlib import Path
-from typing import Any, Union
+from typing import TYPE_CHECKING, Any, Union
 
 import vyper
 from vyper.cli.vyper_compile import get_search_paths
-from vyper.compiler.input_bundle import FileInput, FilesystemInputBundle
+from vyper.compiler.input_bundle import (
+    ABIInput,
+    CompilerInput,
+    FileInput,
+    FilesystemInputBundle,
+)
 from vyper.compiler.phases import CompilerData
+from vyper.semantics.types.module import ModuleT
+from vyper.utils import sha256sum
 
 from boa.contracts.abi.abi_contract import ABIContractFactory
 from boa.contracts.vyper.compiler_utils import anchor_compiler_settings
@@ -22,6 +29,9 @@ from boa.contracts.vyper.vyper_contract import (
 from boa.explorer import fetch_abi_from_etherscan
 from boa.util.abi import Address
 from boa.util.disk_cache import DiskCache
+
+if TYPE_CHECKING:
+    from vyper.semantics.analysis.base import ImportInfo
 
 _Contract = Union[VyperContract, VyperBlueprint]
 
@@ -82,31 +92,66 @@ class BoaLoader(SourceFileLoader):
 sys.meta_path.append(BoaImporter())
 
 
+def hash_input(compiler_input: CompilerInput) -> str:
+    if isinstance(compiler_input, FileInput):
+        return compiler_input.sha256sum
+    if isinstance(compiler_input, ABIInput):
+        return sha256sum(str(compiler_input.abi))
+    raise RuntimeError(f"bad compiler input {compiler_input}")
+
+
+# compute a fingerprint for a module which changes if any of its
+# dependencies change
+# TODO consider putting this in its own module
+def get_module_fingerprint(
+    module_t: ModuleT, seen: dict["ImportInfo", str] = None
+) -> str:
+    seen = seen or {}
+    fingerprints = []
+    for stmt in module_t.import_stmts:
+        import_info = stmt._metdata["import_info"]
+        if import_info not in seen:
+            if isinstance(import_info.typ, ModuleT):
+                fingerprint = get_module_fingerprint(import_info.typ, seen)
+            else:
+                fingerprint = hash_input(import_info.compiler_input)
+            seen[import_info] = fingerprint
+        fingerprint = seen[import_info]
+        fingerprints.append(fingerprint)
+    fingerprints.append(module_t._module.source_sha256sum)
+
+    return sha256sum("".join(fingerprints))
+
+
 def compiler_data(
     source_code: str, contract_name: str, filename: str, **kwargs
 ) -> CompilerData:
     global _disk_cache, _search_path
 
-    # TODO: figure out how caching works with modules.
-    if True:
-        file_input = FileInput(
-            source_code=source_code,
-            source_id=-1,
-            path=Path(contract_name),
-            resolved_path=Path(contract_name),
-        )
-        search_paths = get_search_paths(_search_path)
-        input_bundle = FilesystemInputBundle(search_paths)
-        return CompilerData(file_input, input_bundle, **kwargs)
+    file_input = FileInput(
+        source_code=source_code,
+        source_id=-1,
+        path=Path(contract_name),
+        resolved_path=Path(filename),
+    )
+    search_paths = get_search_paths(_search_path)
+    input_bundle = FilesystemInputBundle(search_paths)
 
-    def func():
-        raise Exception("unreachable")
-        ret = CompilerData(source_code, contract_name, **kwargs)
-        with anchor_compiler_settings(ret):
-            _ = ret.bytecode, ret.bytecode_runtime  # force compilation to happen
+    ret = CompilerData(file_input, input_bundle, **kwargs)
+    if _disk_cache is None:
         return ret
 
-    return _disk_cache.caching_lookup(str((kwargs, source_code)), func)
+    with anchor_compiler_settings(ret):
+        module_t = ret.annotated_vyper_module._metadata["type"]
+    fingerprint = get_module_fingerprint(module_t)
+
+    def get_compiler_data():
+        with anchor_compiler_settings(ret):
+            # force compilation to happen so DiskCache will cache the compiled artifact:
+            _ = ret.bytecode, ret.bytecode_runtime
+        return ret
+
+    return _disk_cache.caching_lookup(str((kwargs, fingerprint)), get_compiler_data)
 
 
 def load(filename: str, *args, **kwargs) -> _Contract:  # type: ignore
