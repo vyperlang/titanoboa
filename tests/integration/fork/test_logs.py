31f355466684c991848adda9c94f6fc708b36291
Carlos Ré <carlos.santos.re@gmail.com>
2024-05-08 13:13:09 +0100

    test RawEvent event_data

--- tests/integration/fork/test_logs.py
+++ tests/integration/fork/test_logs.py
@@ -1,13 +1,17 @@
 import pytest
 
 import boa
+from boa.contracts.vyper.event import RawEvent
+from vyper.utils import keccak256
 
-code = """
+WETH_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+
+code = f"""
 
 interface IWETH:
     def deposit(): payable
 
-weth9: constant(address) = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
+weth9: constant(address) = {WETH_ADDRESS}
 
 @external
 @payable
@@ -24,11 +28,17 @@ def simple_contract():
 
 def test_logs(simple_contract):
     eoa = boa.env.generate_address()
-    boa.env.set_balance(eoa, 10**21)
-    simple_contract.deposit(value=10**18, sender=eoa)
+    amount = 10**18
+    boa.env.set_balance(eoa, amount)
+    simple_contract.deposit(value=amount, sender=eoa)
+
+    topic0 = keccak256("Deposit(address,uint256)".encode())
+    expect_raw_log = (
+        0,
+        int(WETH_ADDRESS, 16).to_bytes(20),
+        (int.from_bytes(topic0), int(simple_contract.address, 16)),
+        amount.to_bytes(32)
+    )
 
     logs = simple_contract.get_logs()
-    assert len(logs) > 0
-
-    for log in logs:
-        assert hasattr(log, "event_data")
+    assert logs == [RawEvent(expect_raw_log)]
