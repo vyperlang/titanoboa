vyperlang/titanoboa

commit a9d0e8ed05365bc0f8762f5a0a5c952da6629a80

create deploy method

boa/contracts/vyper/vyper_contract.py
@@ -14,7 +14,7 @@ import vyper.ast as vy_ast
 import vyper.ir.compile_ir as compile_ir
 import vyper.semantics.analysis as analysis
 import vyper.semantics.namespace as vy_ns
-from eth.exceptions import Revert, VMError
+from eth.exceptions import VMError
 from vyper.ast.utils import parse_to_ast
 from vyper.codegen.core import anchor_opt_level, calculate_type_for_external_return
 from vyper.codegen.function_definitions import generate_ir_for_function
@@ -528,18 +528,16 @@ class VyperContract(_BaseVyperContract):
             encoded_args = self._ctor.prepare_calldata(*args)
 
         initcode = self.compiler_data.bytecode + encoded_args
-        try:
-            addr, self.bytecode = self.env.deploy_code(
-                bytecode=initcode, value=value, override_address=override_address
-            )
-        except Revert:
-            raise BoaError(self.stack_trace(self.env._last_computation))
+        computation = self.env.deploy(
+            bytecode=initcode, value=value, override_address=override_address
+        )
+        self._computation = computation
 
-        if addr.canonical_address not in self.env._last_computation.contracts_created:
-            raise ValueError("contract creation failed")
+        if computation.is_error:
+            raise BoaError(self.stack_trace(computation))
 
-        self._computation = self.env._last_computation
-        return Address(addr)
+        (address,) = computation.contracts_created
+        return Address(address)
 
     # manually set the runtime bytecode, instead of using deploy
     def _set_bytecode(self, bytecode: bytes) -> None:

boa/environment.py
@@ -36,7 +36,6 @@ class Env:
 
         self._contracts = {}
         self._code_registry = {}
-        self._last_computation = None
 
         self.sha3_trace: dict = {}
         self.sstore_trace: dict = {}
@@ -195,7 +194,7 @@ class Env:
     def _update_gas_used(self, gas_used: int):
         self._gas_tracker += gas_used
 
-    def deploy_code(
+    def deploy(
         self,
         sender: Optional[_AddressType] = None,
         gas: Optional[int] = None,
@@ -204,7 +203,7 @@ class Env:
         start_pc: int = 0,  # TODO: This isn't used
         # override the target address:
         override_address: Optional[_AddressType] = None,
-    ) -> tuple[Address, bytes]:
+    ):
         sender = self._get_sender(sender)
 
         if override_address is None:
@@ -213,7 +212,7 @@ class Env:
             target_address = Address(override_address)
 
         origin = sender  # XXX: consider making this parameterizable
-        c = self.evm.deploy_code(
+        computation = self.evm.deploy_code(
             sender=sender,
             origin=origin,
             target_address=target_address,
@@ -222,14 +221,18 @@ class Env:
             value=value,
             bytecode=bytecode,
         )
-        self._last_computation = c
-        if c._gas_meter_class != NoGasMeter:
-            self._update_gas_used(c.get_gas_used())
 
-        if c.is_error:
-            raise c.error
+        if computation._gas_meter_class != NoGasMeter:
+            self._update_gas_used(computation.get_gas_used())
+        return computation
 
-        return target_address, c.output
+    def deploy_code(self, *args, **kwargs) -> tuple[Address, bytes]:
+        computation = self.deploy(*args, **kwargs)
+        if computation.is_error:
+            raise computation.error
+
+        (address,) = computation.contracts_created
+        return Address(address), computation.output
 
     def raw_call(
         self,

commit 2ceafa5f973ea4f8b249e34fe3cccfe19f4547bd

fix: add missing bytecode
seems to be a regression in a9d0e8ed05365b

boa/contracts/vyper/vyper_contract.py
@@ -536,6 +536,7 @@ class VyperContract(_BaseVyperContract):
             bytecode=initcode, value=value, override_address=override_address
         )
         self._computation = computation
+        self.bytecode = computation.output
 
         if computation.is_error:
             raise BoaError(self.stack_trace(computation))