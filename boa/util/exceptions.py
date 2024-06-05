5d9175d38f1dcfe056286123a79b776a513bce63
Daniel Schiavini <daniel.schiavini@gmail.com>
2024-06-05 15:41:42 +0200

    use __spec__.parent + add comments

--- boa/util/exceptions.py
+++ boa/util/exceptions.py
@@ -4,15 +4,19 @@ import types
 
 # take an exception instance, and strip frames in the target module
 # from the traceback
-def strip_internal_frames(exc, module_name=None):
+def strip_internal_frames(exc, package_name=None):
     error_type, error, traceback = sys.exc_info()
     frame = traceback.tb_frame
 
-    if module_name is None:
+    if package_name is None:
         # use the parent module of the module where the exception was raised
-        module_name = frame.f_globals["__name__"].rsplit(".", 1)[0]
+        # currently, package_name is always `boa.contracts`
+        package_name = frame.f_globals["__spec__"].parent
 
-    while frame.f_globals.get("__name__", "").startswith(module_name):
+    # check with startswith because the package name may be a subpackage
+    # currently, frame.f_globals["__spec__"].parent is always
+    # `boa.contracts.vyper`, `boa.contracts.abi` or `boa.contracts`
+    while frame.f_globals["__spec__"].parent.startswith(package_name):
         frame = frame.f_back
 
     # kwargs incompatible with pypy here
