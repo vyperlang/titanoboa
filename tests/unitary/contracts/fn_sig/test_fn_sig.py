2d8ac3868cd5337fa621b14cc8de959a5a8359d2
bout3fiddy <11488427+bout3fiddy@users.noreply.github.com>
2024-07-17 17:56:22 +0200

    add test to check issue

--- tests/unitary/contracts/fn_sig/test_fn_sig.py
+++ tests/unitary/contracts/fn_sig/test_fn_sig.py
@@ -3,3 +3,7 @@ import boa
 
 def test_load_fn_sig():
     pass
+    # fn = boa.loads_fn_sig("def balanceOf(a: address) -> uint256: view")
+    # boa.env.fork("https://eth.llamarpc.com")
+    # crv = fn.at("0xD533a949740bb3306d119CC777fa900bA034cd52")
+    # crv.balanceOf('0x5f3b5DfEb7B28CDbD7FAba78963EE202a494e2A2')
