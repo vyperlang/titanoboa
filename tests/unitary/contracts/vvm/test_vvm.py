e8431e0724ad2b8502888db269a57cf2e475ed10
Alberto <albicento.ac@gmail.com>
2024-08-02 16:55:24 -0700

    test: vvm compilation with old version

---
+++ tests/unitary/contracts/vvm/test_vvm.py
@@ -0,0 +1,34 @@
+import boa
+
+
+def test_load_partial_vvm():
+    contract_deployer = boa.load_partial_vvm("mock_3_10.vy", "0.3.10")
+    contract = contract_deployer.deploy(43)
+
+    assert contract.foo() == 42
+    assert contract.bar() == 43
+
+def test_loads_partial_vvm():
+    with open("mock_3_10.vy") as f:
+        code = f.read()
+
+    contract_deployer = boa.loads_partial_vvm(code, "0.3.10")
+    contract = contract_deployer.deploy(43)
+
+    assert contract.foo() == 42
+    assert contract.bar() == 43
+
+def test_load_vvm():
+    contract = boa.load_vvm("mock_3_10.vy", "0.3.10", 43)
+
+    assert contract.foo() == 42
+    assert contract.bar() == 43
+    
+def test_loads_vvm():
+    with open("mock_3_10.vy") as f:
+        code = f.read()
+
+    contract = boa.loads_vvm(code, "0.3.10", 43)
+
+    assert contract.foo() == 42
+    assert contract.bar() == 43
