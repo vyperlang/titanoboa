58e21249bdd0a122e5444042b2ad2da4c4a8eb71
Alberto <albicento.ac@gmail.com>
2024-08-02 18:02:45 -0700

    test: corrected path

--- tests/unitary/contracts/vvm/test_vvm.py
+++ tests/unitary/contracts/vvm/test_vvm.py
@@ -1,8 +1,9 @@
 import boa
 
+mock_3_10_path = "tests/unitary/contracts/vvm/mock_3_10.vy"
 
 def test_load_partial_vvm():
-    contract_deployer = boa.load_partial_vvm("contracts/vvm/mock_3_10.vy", "0.3.10")
+    contract_deployer = boa.load_partial_vvm(mock_3_10_path, "0.3.10")
     contract = contract_deployer.deploy(43)
 
     assert contract.foo() == 42
@@ -10,7 +11,7 @@ def test_load_partial_vvm():
 
 
 def test_loads_partial_vvm():
-    with open("contracts/vvm/mock_3_10.vy") as f:
+    with open(mock_3_10_path) as f:
         code = f.read()
 
     contract_deployer = boa.loads_partial_vvm(code, "0.3.10")
@@ -21,14 +22,14 @@ def test_loads_partial_vvm():
 
 
 def test_load_vvm():
-    contract = boa.load_vvm("contracts/vvm/mock_3_10.vy", "0.3.10", 43)
+    contract = boa.load_vvm(mock_3_10_path, "0.3.10", 43)
 
     assert contract.foo() == 42
     assert contract.bar() == 43
 
 
 def test_loads_vvm():
-    with open("contracts/vvm/mock_3_10.vy") as f:
+    with open(mock_3_10_path) as f:
         code = f.read()
 
     contract = boa.loads_vvm(code, "0.3.10", 43)
