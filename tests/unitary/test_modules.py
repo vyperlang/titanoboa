b5a4d7e578086cf7ede58dd878340a58fac5aa7a
Charles Cooper <cooper.charles.m@gmail.com>
2024-08-05 13:34:18 +0800

    fix lint

--- tests/unitary/test_modules.py
+++ tests/unitary/test_modules.py
@@ -1,17 +1,21 @@
-import pytest
 from pathlib import Path
+
+import pytest
 
 import boa
 
 FIXTURES = Path(__file__).parent / "fixtures"
 
+
 @pytest.fixture
 def module_contract():
     return boa.load(FIXTURES / "module_contract.vy")
 
+
 def test_user_raise(module_contract):
     with boa.reverts("Error with message"):
         module_contract.fail()
+
 
 def test_dev_reason(module_contract):
     with boa.reverts("some dev reason"):
