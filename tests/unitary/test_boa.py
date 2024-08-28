70afe9578ff9571bb04c34a6e290813bb0d94ff7
Charles Cooper <cooper.charles.m@gmail.com>
2024-08-28 12:45:18 +0800

    fix lint

--- tests/unitary/test_boa.py
+++ tests/unitary/test_boa.py
@@ -1,11 +1,13 @@
 import boa
 
+
 def test_env_mgr_noctx():
     s = boa.env
     t = boa.Env()
     boa._TmpEnvMgr(t)
     assert boa.env is not s
     assert boa.env is t
+
 
 def test_env_mgr_with_ctx():
     s = boa.env
