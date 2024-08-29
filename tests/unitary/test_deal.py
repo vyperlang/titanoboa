3f22cc94c76d3140a156f64a2e4245fd78564435
Alberto <albicento.ac@gmail.com>
2024-08-29 14:40:05 +0200

    test: correct tests to account for supply

--- tests/unitary/test_deal.py
+++ tests/unitary/test_deal.py
@@ -4,28 +4,52 @@ import boa
 
 
 def test_simple_mapping_zero_balance():
-    source = "balanceOf: public(HashMap[address, uint256])"
+    source = """
+    balanceOf: public(HashMap[address, uint256])
+    totalSupply: public(uint256)
+    """
 
     contract = boa.loads(source)
 
     boa.deal(contract, 100, receiver := boa.env.generate_address())
 
     assert contract.balanceOf(receiver) == 100
+    assert contract.totalSupply() == 100
+
+
+def test_simple_mapping_no_supply_adjustment():
+    source = """
+    balanceOf: public(HashMap[address, uint256])
+    totalSupply: public(uint256)
+    """
+
+    contract = boa.loads(source)
+
+    boa.deal(contract, 100, receiver := boa.env.generate_address(), adjust_supply=False)
+
+    assert contract.balanceOf(receiver) == 100
+    assert contract.totalSupply() == 0
 
 
 def test_simple_mapping_non_zero_balance():
-    source = "balanceOf: public(HashMap[address, uint256])"
-
+    source = """
+    balanceOf: public(HashMap[address, uint256])
+    totalSupply: public(uint256)
+    """
     contract = boa.loads(source)
 
     receiver = boa.env.generate_address()
 
     contract.eval(f"self.balanceOf[{receiver}] = 120")
+    contract.eval("self.totalSupply = 120")
+
     assert contract.balanceOf(receiver) == 120
+    assert contract.totalSupply() == 120
 
     boa.deal(contract, 100, receiver)
 
     assert contract.balanceOf(receiver) == 100
+    assert contract.totalSupply() == 100
 
 
 def test_multiple_sloads_same_value():
@@ -38,6 +62,7 @@ def test_multiple_sloads_same_value():
     def balanceOf(receiver: address) -> uint256:
         foobar: uint256 = self.bar + self.foo
         return self.foo
+    totalSupply: public(uint256)
     """
 
     contract = boa.loads(source)
@@ -45,12 +70,14 @@ def test_multiple_sloads_same_value():
     boa.deal(contract, 100, receiver := boa.env.generate_address())
 
     assert contract.balanceOf(receiver) == 100
+    assert contract.totalSupply() == 100
 
 
 def test_vvm_contract():
     source = """
     # pragma version 0.3.10
     balanceOf: public(HashMap[address, uint256])
+    totalSupply: public(uint256)
     """
 
     contract = boa.loads(source)
@@ -70,16 +97,51 @@ def test_deal_failure_non_erc20():
     contract = boa.loads(source)
 
     with pytest.raises(
-        ValueError, match="Invalid token contract, are you sure it's an ERC20?"
+        ValueError, match="Invalid erc20 contract, function balanceOf not found"
     ):
         boa.deal(contract, 100, boa.env.generate_address())
 
 
-def test_deal_failure_exotic_token():
+def test_deal_failure_non_erc20_totalSupply():
+    source = """
+    foo: public(uint256)
+    balanceOf: public(HashMap[address, uint256])
+    """
+
+    contract = boa.loads(source)
+
+    with pytest.raises(
+        ValueError, match="Invalid erc20 contract, function totalSupply not found"
+    ):
+        boa.deal(contract, 100, boa.env.generate_address())
+
+
+def test_deal_failure_exotic_token_balanceOf():
     code = """
     @external
     @view
     def balanceOf(receiver: address) -> uint256:
+        return 100
+
+    totalSupply: public(uint256)
+    """
+
+    contract = boa.loads(code)
+
+    with pytest.raises(
+        ValueError,
+        match="Could not find the target slot for balanceOf, this is expected if"
+        " the token packs storage slots or computes the value on the fly",
+    ):
+        boa.deal(contract, 100, boa.env.generate_address())
+
+
+def test_deal_failure_exotic_token_totalSupply():
+    code = """
+    balanceOf: public(HashMap[address, uint256])
+    @external
+    @view
+    def totalSupply() -> uint256:
         return 100
     """
 
@@ -87,7 +149,7 @@ def test_deal_failure_exotic_token():
 
     with pytest.raises(
         ValueError,
-        match="Could not find the target slot, this is expected if the token "
-        "packs storage slots or computes the balance on the fly",
+        match="Could not find the target slot for totalSupply, this is expected if"
+        " the token packs storage slots or computes the value on the fly",
     ):
         boa.deal(contract, 100, boa.env.generate_address())
