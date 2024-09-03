e5300e67ef1c06de6cd9b2b7b735549ee9cfe40a
Alberto <albicento.ac@gmail.com>
2024-09-03 15:36:01 +0200

    test: caching for vvm contracts

--- tests/unitary/utils/test_cache.py
+++ tests/unitary/utils/test_cache.py
@@ -1,7 +1,9 @@
+from unittest.mock import patch
 from vyper.compiler import CompilerData
 
 from boa.contracts.vyper.vyper_contract import VyperDeployer
-from boa.interpret import _disk_cache, compiler_data
+from boa.contracts.vvm.vvm_contract import VVMDeployer
+from boa.interpret import _disk_cache, compiler_data, _loads_partial_vvm
 
 
 def test_cache_contract_name():
@@ -15,6 +17,47 @@ x: constant(int128) = 1000
     assert _to_dict(test1) == _to_dict(test3), "Should hit the cache"
     assert _to_dict(test1) != _to_dict(test2), "Should be different objects"
     assert str(test2.contract_path) == "test2"
+
+
+def test_cache_vvm():
+    code = """
+# @version 0.2.8
+x: constant(int128) = 1000
+"""
+    version = "0.2.8"
+    assert _disk_cache is not None
+
+    # Clear the cache before the test
+    _disk_cache.gc(force=True)
+
+    # Mock both vvm.compile_source and vvm.install_vyper
+    with patch('vvm.compile_source') as mock_compile, patch('vvm.install_vyper') as mock_install:
+        # Set up the mock to return a valid compiler output
+        mock_compile.return_value = {
+            "<stdin>": {
+                "abi": [],
+                "bytecode": "0x1234",
+            }
+        }
+
+        # First call should compile
+        test1 = _loads_partial_vvm(code, version, __file__)
+        assert mock_install.call_count == 1, "vvm.install_vyper should be called once"
+        assert mock_compile.call_count == 1, "vvm.compile_source should be called once"
+
+        # Second call should hit the cache
+        test2 = _loads_partial_vvm(code, version, __file__)
+        assert mock_install.call_count == 1, "vvm.install_vyper should not be called again"
+        assert mock_compile.call_count == 1, "vvm.compile_source should not be called again"
+
+        # Third call should also hit the cache
+        test3 = _loads_partial_vvm(code, version, __file__)
+        assert mock_install.call_count == 1, "vvm.install_vyper should not be called again"
+        assert mock_compile.call_count == 1, "vvm.compile_source should not be called again"
+
+    assert test1.abi == test2.abi == test3.abi, "ABI should be the same (from cache)"
+    assert test1.bytecode == test2.bytecode == test3.bytecode, "Bytecode should be the same (from cache)"
+    assert test1.filename == test2.filename == test3.filename, "Filename should be the same"
 
 
 def _to_dict(data: CompilerData) -> dict:
