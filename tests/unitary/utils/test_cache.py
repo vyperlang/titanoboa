39f9bc886243506746b068455eb84b7fd147e1b3
Charles Cooper <cooper.charles.m@gmail.com>
2024-09-05 16:10:13 +0800

    polish tests

--- tests/unitary/utils/test_cache.py
+++ tests/unitary/utils/test_cache.py
@@ -37,36 +37,27 @@ x: constant(int128) = 1000
         # Set up the mock to return a valid compiler output
         mock_compile.return_value = {"<stdin>": {"abi": [], "bytecode": "0x1234"}}
 
-        # First call should compile
-        test1 = _loads_partial_vvm(code, version, __file__)
-        assert mock_install.call_count == 1, "vvm.install_vyper should be called once"
-        assert mock_compile.call_count == 1, "vvm.compile_source should be called once"
+        assert mock_install.call_count == 0
+        assert mock_compile.call_count == 0
+
+        # First call should hit vvm.compile_source
+        test1 = _loads_partial_vvm(code, version, "fake_file.vy")
+        assert mock_install.call_count == 1
+        assert mock_compile.call_count == 1
 
         # Second call should hit the cache
-        test2 = _loads_partial_vvm(code, version, __file__)
-        assert (
-            mock_install.call_count == 1
-        ), "vvm.install_vyper should not be called again"
-        assert (
-            mock_compile.call_count == 1
-        ), "vvm.compile_source should not be called again"
+        test2 = _loads_partial_vvm(code, version, "fake_file.vy")
+        assert mock_install.call_count == 2
+        assert mock_compile.call_count == 1
 
-        # Third call should also hit the cache
-        test3 = _loads_partial_vvm(code, version, __file__)
-        assert (
-            mock_install.call_count == 1
-        ), "vvm.install_vyper should not be called again"
-        assert (
-            mock_compile.call_count == 1
-        ), "vvm.compile_source should not be called again"
+        # Third call should also hit the cache!
+        test3 = _loads_partial_vvm(code, version, "fake_file.vy")
+        assert mock_install.call_count == 3
+        assert mock_compile.call_count == 1
 
-    assert test1.abi == test2.abi == test3.abi, "ABI should be the same (from cache)"
-    assert (
-        test1.bytecode == test2.bytecode == test3.bytecode
-    ), "Bytecode should be the same (from cache)"
-    assert (
-        test1.filename == test2.filename == test3.filename
-    ), "Filename should be the same"
+    assert test1.abi == test2.abi == test3.abi
+    assert test1.bytecode == test2.bytecode == test3.bytecode
+    assert test1.filename == test2.filename == test3.filename
 
 
 def _to_dict(data: CompilerData) -> dict:
