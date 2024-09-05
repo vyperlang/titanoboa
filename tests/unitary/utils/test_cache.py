ea4e1736ca9415b2a5dd89fed728cb69749e3792
Charles Cooper <cooper.charles.m@gmail.com>
2024-09-05 16:34:00 +0800

    fix up tests - don't bother mocking vvm_install, don't zonk global disk cache

--- tests/unitary/utils/test_cache.py
+++ tests/unitary/utils/test_cache.py
@@ -1,9 +1,20 @@
 from unittest.mock import patch
 
+import pytest
 from vyper.compiler import CompilerData
 
 from boa.contracts.vyper.vyper_contract import VyperDeployer
-from boa.interpret import _disk_cache, _loads_partial_vvm, compiler_data
+from boa.interpret import _disk_cache, _loads_partial_vvm, compiler_data, set_cache_dir
+
+
+@pytest.fixture(autouse=True)
+def cache_dir(tmp_path):
+    tmp = _disk_cache.cache_dir
+    try:
+        set_cache_dir(tmp_path)
+        yield
+    finally:
+        set_cache_dir(tmp)
 
 
 def test_cache_contract_name():
@@ -21,43 +32,38 @@ x: constant(int128) = 1000
 
 def test_cache_vvm():
     code = """
-# @version 0.2.8
 x: constant(int128) = 1000
 """
     version = "0.2.8"
+    version2 = "0.3.1"
     assert _disk_cache is not None
 
-    # Clear the cache before the test
-    _disk_cache.gc(force=True)
-
-    # Mock both vvm.compile_source and vvm.install_vyper
-    with patch("vvm.compile_source") as mock_compile, patch(
-        "vvm.install_vyper"
-    ) as mock_install:
+    # Mock vvm.compile_source
+    with patch("vvm.compile_source") as mock_compile:
         # Set up the mock to return a valid compiler output
         mock_compile.return_value = {"<stdin>": {"abi": [], "bytecode": "0x1234"}}
 
-        assert mock_install.call_count == 0
         assert mock_compile.call_count == 0
 
         # First call should hit vvm.compile_source
         test1 = _loads_partial_vvm(code, version, "fake_file.vy")
-        assert mock_install.call_count == 1
         assert mock_compile.call_count == 1
 
         # Second call should hit the cache
         test2 = _loads_partial_vvm(code, version, "fake_file.vy")
-        assert mock_install.call_count == 2
         assert mock_compile.call_count == 1
 
-        # Third call should also hit the cache!
-        test3 = _loads_partial_vvm(code, version, "fake_file.vy")
-        assert mock_install.call_count == 3
+        # using a different filename should also hit the cache
+        test3 = _loads_partial_vvm(code, version, "fake_fileeeee.vy")
         assert mock_compile.call_count == 1
+
+        # using a different vyper version should *miss* the cache
+        _loads_partial_vvm(code, version2, "fake_file.vy")
+        assert mock_compile.call_count == 2
 
     assert test1.abi == test2.abi == test3.abi
     assert test1.bytecode == test2.bytecode == test3.bytecode
-    assert test1.filename == test2.filename == test3.filename
+    assert test1.filename == test2.filename
 
 
 def _to_dict(data: CompilerData) -> dict:
