21a8620c6858a4adbe32d86260c26c72caf59757
Hubert Ritzdorf <hubert.ritzdorf@chainsecurity.com>
2024-09-17 03:23:34 +0200

    Fix for VVM with non-fixed version pragma

--- boa/contracts/vvm/vvm_contract.py
+++ boa/contracts/vvm/vvm_contract.py
@@ -1,20 +1,58 @@
 import re
 from functools import cached_property
 
+from vvm.utils.convert import to_vyper_version
 from boa.contracts.abi.abi_contract import ABIContractFactory, ABIFunction
 from boa.environment import Env
 
 # TODO: maybe this doesn't detect release candidates
-VERSION_RE = re.compile(r"\s*#\s*(pragma\s+version|@version)\s+(\d+\.\d+\.\d+)")
+VERSION_RE = re.compile(r"\s*#\s*(pragma\s+version|@version)\s+(\^|>=)?(\d+\.\d+\.\d+)")
 
 
 # TODO: maybe move this up to vvm?
-def _detect_version(source_code: str):
+# 1. Detects version from source file
+# 2. In case of ^ or >= and if vyper_version is provided, returns vyper_version is allowed
+def _detect_version(source_code: str, vyper_version: str = None) -> str:
     res = VERSION_RE.findall(source_code)
     if len(res) < 1:
         return None
     # TODO: handle len(res) > 1
-    return res[0][1]
+
+    # Exact version specified
+    if res[0][1] == "":
+        return res[0][2]
+    # Caret means we check if vyper_version is compatible
+    elif res[0][1] == "^":
+        if vyper_version:
+            min_version = to_vyper_version(res[0][2])
+            # Compute maximum version according to rules
+            min_nonallowed_version = to_vyper_version(
+                f"0.{min_version.minor+1}.0"
+                if min_version.major == 0
+                else f"{min_version.major+1}.0.0"
+            )
+            vy_version = to_vyper_version(vyper_version)
+            if min_version <= vy_version and vy_version < min_nonallowed_version:
+                return vyper_version
+            else:
+                # Else use minimum allowed version
+                return res[0][2]
+        else:
+            return res[0][2]
+    # Greater-Equal means we check if vyper_version is compatible
+    elif res[0][1] == ">=":
+        # If vyper_version is allowed by >=, use it
+        if vyper_version:
+            if to_vyper_version(vyper_version) >= to_vyper_version(res[0][2]):
+                return vyper_version
+            else:
+                # Else use minimum allowed version
+                return res[0][2]
+        else:
+            return res[0][2]
+    # Unknown Operand
+    else:
+        return None
 
 
 class VVMDeployer:
