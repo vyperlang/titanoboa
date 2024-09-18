9efd375759e8551c07e3618ba3e15a69a7ac8754
Charles Cooper <cooper.charles.m@gmail.com>
2024-09-18 15:56:25 -0400

    fix lint

--- tests/unitary/contracts/vyper/test_vyi.py
+++ tests/unitary/contracts/vyper/test_vyi.py
@@ -1,4 +1,5 @@
 import pytest
+
 import boa
 
 FOO_CONTRACT = """
@@ -13,13 +14,16 @@ def foo() -> uint256:
     ...
 """
 
+
 @pytest.fixture
 def foo_contract():
     return boa.loads(FOO_CONTRACT)
 
+
 @pytest.fixture
 def foo_interface(foo_contract):
     return boa.loads_vyi(FOO_INTERFACE).at(foo_contract.address)
+
 
 # from file
 @pytest.fixture
@@ -32,6 +36,7 @@ def foo_interface2(foo_contract, tmp_path):
 
 def test_foo_interface(foo_interface):
     assert foo_interface.foo() == 5
+
 
 def test_foo_interface2(foo_interface2):
     assert foo_interface2.foo() == 5
