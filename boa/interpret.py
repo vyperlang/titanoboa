f8cfc7f6a084c5b18d0586d1fcc0f85a40a9d2e5
Charles Cooper <cooper.charles.m@gmail.com>
2025-01-26 20:56:41 -0500

    add support for vyper 0.4.1
    - update calls to analyze_module since import resolution is now a
      separate step (vyper PR 4229)
    - add no_vvm= kwarg to loads and loads_partial (to avoid using vvm)
    - fn_ast.args.node_source_code no longer works after ASTTokens
      refactor (vyper PR 4364), so something had to be handrolled
    - some equality-based unit tests no longer work (probably after removing
      recursive `VyperNode.__eq__()` implementation, vyper PR 4433)

--- boa/interpret.py
+++ boa/interpret.py
@@ -1,6 +1,9 @@
 import sys
+from vyper.semantics.analysis.imports import resolve_imports
+
 import textwrap
 from importlib.abc import MetaPathFinder
+import contextlib
 from importlib.machinery import SourceFileLoader
 from importlib.util import spec_from_loader
 from pathlib import Path
@@ -190,9 +193,10 @@ def loads(
     name=None,
     filename=None,
     compiler_args=None,
+    no_vvm=False,
     **kwargs,
 ):
-    d = loads_partial(source_code, name, filename=filename, compiler_args=compiler_args)
+    d = loads_partial(source_code, name, filename=filename, compiler_args=compiler_args, no_vvm=no_vvm)
     if as_blueprint:
         return d.deploy_as_blueprint(contract_name=name, **kwargs)
     else:
@@ -222,10 +226,12 @@ def load_vyi(filename: str, name: str = None) -> ABIContractFactory:
 
 
 # load interface from .vyi file string contents.
+# NOTE: since vyi files can be compiled in 0.4.1, this codepath can probably
+# be refactored to use CompilerData (or straight loads_partial)
 def loads_vyi(source_code: str, name: str = None, filename: str = None):
     global _search_path
 
-    ast = parse_to_ast(source_code)
+    ast = parse_to_ast(source_code, is_interface=True)
 
     if name is None:
         name = "VyperContract.vyi"
@@ -233,7 +239,15 @@ def loads_vyi(source_code: str, name: str = None, filename: str = None):
     search_paths = get_search_paths(_search_path)
     input_bundle = FilesystemInputBundle(search_paths)
 
-    module_t = analyze_module(ast, input_bundle, is_interface=True)
+    # cf. CompilerData._resolve_imports
+    if filename is not None:
+        ctx = input_bundle.search_path(Path(filename).parent)
+    else:
+        ctx = contextlib.nullcontext()
+    with ctx:
+        imports = resolve_imports(ast, input_bundle)
+
+    module_t = analyze_module(ast)
     abi = module_t.interface.to_toplevel_abi_dict()
     return ABIContractFactory(name, abi, filename=filename)
 
@@ -244,6 +258,7 @@ def loads_partial(
     filename: str | Path | None = None,
     dedent: bool = True,
     compiler_args: dict = None,
+    no_vvm: bool = False,
 ) -> VyperDeployer:
     if filename is None:
         filename = "<unknown>"
@@ -251,12 +266,13 @@ def loads_partial(
     if dedent:
         source_code = textwrap.dedent(source_code)
 
-    specifier_set = detect_version_specifier_set(source_code)
-    # Use VVM only if the installed version is not in the specifier set
-    if specifier_set is not None and not specifier_set.contains(vyper.__version__):
-        version = _pick_vyper_version(specifier_set)
-        filename = str(filename)  # help mypy
-        return _loads_partial_vvm(source_code, version, name, filename)
+    if not no_vvm:
+        specifier_set = detect_version_specifier_set(source_code)
+        # Use VVM only if the installed version is not in the specifier set
+        if specifier_set is not None and not specifier_set.contains(vyper.__version__):
+            version = _pick_vyper_version(specifier_set)
+            filename = str(filename)  # help mypy
+            return _loads_partial_vvm(source_code, version, name, filename)
 
     compiler_args = compiler_args or {}
 
